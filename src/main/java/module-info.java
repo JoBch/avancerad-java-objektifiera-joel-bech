cd79205be24cab066533c32dcf7929d5fcd81d4b
Joel Bech <122796634+JoBch@users.noreply.github.com>
2023-12-01 20:46:03 +0100

    Fixed gson not being used anymore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.avanceradjavaobjektifierajoelbech {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires com.google.gson;
 
 
     opens com.example.avanceradjavaobjektifierajoelbech to javafx.fxml;
